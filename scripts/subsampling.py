a112cbd19ef3f2b95f6bbab1d056697e6bbf0324
Hanyang Hu <63213964+KoHomerHu@users.noreply.github.com>
2024-10-24 00:05:51 +0800

    Remove useless files

---
+++ scripts/subsampling.py
@@ -0,0 +1,9 @@
+import torch
+
+
+def farthest_point_sampling(X, n_samples):
+    pass
+
+
+def anchor_net(X, n_samples):
+    pass
---
+++ scripts/utils.py
@@ -0,0 +1,7 @@
+import torch
+
+"""
+Generate low-discrepancy Halton sequence in a rectangular box.
+"""
+def generate_halton_sequence(size, bounds=None, device=None):
+    pass
